2fdb7177cb483d15816579160a07e6d91a56d13e
grey-mountain <greymountain.nvim@gmail.com>
2023-11-06 16:55:44 +0300

    message

---
+++ nvim/.config/astronvim/lua/user/plugins/diffview.lua
@@ -0,0 +1,10 @@
+return {
+    keys = {
+        { "<leader>gvo", "<cmd>DiffviewOpen<cr>", desc = "Open diffview" },
+        { "<leader>gvc", "<cmd>DiffviewClose<cr>", desc = "Close diffview" },
+        { "<leader>gvf", "<cmd>DiffviewFileHistory<cr>", desc = "Open file history" },
+        { "<leader>gvt", "<cmd>DiffviewToggleFiles<cr>", desc = "Toggle file panel" }
+    },
+    "sindrets/diffview.nvim",
+    lazy = false,
+}
