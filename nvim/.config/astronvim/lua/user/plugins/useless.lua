7b9c8ce8e5f74c1ee2bc85a761029af955791dcb
grey-mountain <greymountain.nvim@gmail.com>
2024-06-22 22:06:39 +0300

    message

---
+++ nvim/.config/astronvim/lua/user/plugins/useless.lua
@@ -0,0 +1,18 @@
+return {
+    {
+        "Febri-i/snake.nvim",
+        dependencies = {
+            "Febri-i/fscreen.nvim"
+        },
+        opts = {},
+        lazy = false,
+    },
+    {
+        "seandewar/killersheep.nvim",
+        lazy = false,
+    },
+    {
+        "eandrju/cellular-automaton.nvim",
+        lazy = false,
+    },
+}
