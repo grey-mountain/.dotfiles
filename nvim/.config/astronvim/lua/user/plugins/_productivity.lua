2a8c94c91e306ec9dcf88f2eb722cadc1db93ca6
grey-mountain <greymountain.nvim@gmail.com>
2024-06-26 20:38:58 +0300

    message

---
+++ nvim/.config/astronvim/lua/user/plugins/_productivity.lua
@@ -0,0 +1,42 @@
+return {
+    -- TIME TRACKING
+    {
+        "wakatime/vim-wakatime",
+        lazy = false,
+    },
+    -- TODO COMMENTS
+    {
+        "folke/todo-comments.nvim",
+        requires = "nvim-lua/plenary.nvim",
+        cmd = {
+            "TodoQuickFix",
+            "TodoTelescope",
+        },
+        config = function()
+            require("todo-comments").setup()
+        end,
+        lazy = false,
+    },
+    -- CHATGPT
+    {
+        "jackMort/ChatGPT.nvim",
+        config = function()
+            require("chatgpt").setup({
+                keymaps = {
+                    submit = "<M-k>",
+                    close = "<M-o>",
+                },
+            })
+        end,
+        keys = {
+            { "<A-o>", "<cmd>ChatGPT<cr>", desc = "Start ChatGPT" },
+        },
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim"
+        },
+        lazy = false,
+        enabled = false,
+    }
+}
---
+++ nvim/.config/astronvim/lua/user/plugins/_text-editing.lua
@@ -0,0 +1,27 @@
+return {
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    },
+    {
+        "kylechui/nvim-surround",
+        version = "*",
+        config = function()
+            require("nvim-surround").setup({})
+        end,
+        lazy = false,
+    },
+    {
+        "andymass/vim-matchup",
+        config = function()
+            vim.g.matchup_matchparen_offscreen = { method = "popup" }
+        end,
+        lazy = false,
+        enabled = false,
+    }
+}
